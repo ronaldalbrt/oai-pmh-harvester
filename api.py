95da65e8933871e0a5b52f324dcd80e025d9e19b
ronaldalbertjr <ronaldalbert1609@gmail.com>
2022-02-02 11:59:32 -0300

    Add new rout

--- api.py
+++ api.py
@@ -14,20 +14,61 @@ def handle_unknown_error(error):
     return response
 
 
+@server.route("/listMetadataFormats", methods=['POST'])
+def listMetadataFormats():
+    input_data = request.get_json()
+    url = input_data['url']
+
+    formats_array = []
+    try:
+        sickle = Sickle(url)
+        formats = sickle.ListMetadataFormats()
+        
+        for e in formats:
+            formats_array.append({
+                'metadataPrefix': e.metadataPrefix,
+                'metadataNamespace': e.metadataNamespace,
+                'schema': e.schema,
+                'raw': e.raw
+            })
+
+        return { 
+            'data': { 
+                'formats': formats_array
+            }
+        }, 200
+
+    except:
+        raise UnknownError('Erro desconhecido', status_code=500) 
+
 @server.route("/listRecords", methods = ['POST'])
 def listRecords():
     input_data = request.get_json()
     url = input_data['url']
     prefix = input_data['prefix']
 
+    start_date = input_data['from']
+    end_date = input_data['until']
+    n_records = input_data['n_records']
     records_array = []
     try:
         sickle = Sickle(url)
-        records = sickle.ListRecords(metadataPrefix=prefix)
+        records = sickle.ListRecords(**{
+            'metadataPrefix': prefix,
+            'from': start_date,
+            'until': end_date
+        })
+        print(records.next().header)
 
         for record in records:
-            records_array.append(record)
-
+            records_array.append({
+                'header': record.header.raw,
+                'deleted': record.deleted,
+                'raw': record.raw
+            })
+            if(len(records_array) >= n_records ):
+                break
+        
         return { 
             'data': { 
                 'records':  records_array
